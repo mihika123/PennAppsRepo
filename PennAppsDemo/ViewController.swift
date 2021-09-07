5cd06f2c39b09143e3083c7c0f69695870e3ceba
mihika123 <mihikaa.57@gmail.com>
2021-09-07 18:29:04 -0400

    starter code

---
+++ PennAppsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PennAppsDemo
+//
+//  Created by Mihikaa Goenka on 9/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+    }
+
+
+}
+
